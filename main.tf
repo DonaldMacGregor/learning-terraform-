685d695c9ce363909b4c798c60536916ba24070d
Donald MacGregor <93742417+DonaldMacGregor@users.noreply.github.com>
2023-06-05 17:01:49 -0400

    fix for missing subnet

--- main.tf
+++ main.tf
@@ -35,6 +35,7 @@ resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -46,7 +47,7 @@ module "blog_sg" {
   version = "5.0.0"
   name    = "blog_new"
 
-  vpc_id              = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
